1cc48587a1e1e6bd437d10de5fe64dd172f42251
vaxad <varadprabhu111@gmail.com>
2023-07-27 19:17:43 +0530

    pr0

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config({path: '.env.local'});
-import bodyParser from "body-parser";
 import express from "express";
-import cors from "cors"
 import mongoose from "mongoose";
 import postRoutes from './routes/posts.js';
 import authRoutes from './routes/auth.js';
-const app=express();
-app.use(bodyParser.json({limit:"30mb", extended:"true"}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:"true"}));
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+import cors from 'cors';
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+const app=express();
+
+app.use(express.json());
+app.use(cors()) // Use this after the variable declaration
 
 app.use('/posts',postRoutes);
-// app.use('/user',userRoutes);
 app.use('/auth',authRoutes);
 app.get("/", (req, res) => {
    res.send("Server is working");
